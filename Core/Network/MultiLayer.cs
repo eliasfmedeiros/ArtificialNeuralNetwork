d84fec1d62983ef01b9e5420b35c7851066491cc
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-05-05 23:52:54 -0300

    feat: add abstract MultiLayer network class
    Introduce base structure for layered neural networks

---
+++ Core/Network/MultiLayer.cs
@@ -0,0 +1,25 @@
+using System.Text;
+
+namespace EliasFM.ArtificialNeuralNet.Core.Network;
+
+public abstract class MultiLayer : INetwork {
+	public abstract INetwork LayerAt(int index);
+	public abstract int LayerCount { get; }
+
+	public double[] Predict(double[] input) {
+		for (int i = 0; i < LayerCount; i++)
+			input = LayerAt(i).Predict(input);
+		return input;
+	}
+
+	public int InputLength => LayerAt(0).InputLength;
+
+	public int OutputLength => LayerAt(LayerCount - 1).OutputLength;
+
+	public override string ToString() {
+		int layers = LayerCount;
+		StringBuilder sb = new($"{GetType().Name}" + '{' + $"Layers[{layers}]:[");
+		for (int i = 0; i < layers; i++) sb.Append($"#{i}:{LayerAt(i)},");
+		return sb.Remove(sb.Length - 1, 1).Append("]}").ToString();
+	}
+}
