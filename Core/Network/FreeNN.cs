6b3932bb5564f8d17795cacc8411144a75a6bc06
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-05-11 23:38:59 -0300

    feat: implement FreeNN prediction flow
    Add neuron access, string representation and prediction methods

--- Core/Network/FreeNN.cs
+++ Core/Network/FreeNN.cs
@@ -8,14 +8,52 @@ public abstract partial class FreeNN : INetwork {
 
 	public abstract int HiddenNeuronCount { get; }
 
+	public abstract HiddenNeuron HiddenNeuronAt(int index);
+
+	public abstract InputDescribedNeuron OutputNeuronAt(int index);
+
+	private double[] BuildNeuronInput(InputDescribedNeuron neuron, double[] networkInput) { // neuronios são sempre acessíveis, mas networkInput só existe durante chamada de método
+		double[] neuronInput = new double[neuron.InputLength];
+		for (int iInput = 0; iInput < neuronInput.Length; iInput++) {
+			InputDescribedNeuron.IInputDescriptor inputDescription = neuron.DescribeInput(iInput);
+			switch (inputDescription.SourceType) {
+				case InputSourceType.HiddenNeuron: neuronInput[iInput] = HiddenNeuronAt(inputDescription.IndexInSource).StoredOutput; break;
+				case InputSourceType.NetworkInput: neuronInput[iInput] = networkInput[inputDescription.IndexInSource]; break;
+				default: neuronInput[iInput] = AuxInputAt(inputDescription.IndexInSource); break;
+			}
+		}
+		return neuronInput;
+	}
+
+	public virtual double AuxInputAt(int indexInSource) => throw new NotImplementedException($"{this}.AuxInputAt({+indexInSource})");
+
 	public double[] Predict(double[] networkInput) {
-		throw new NotImplementedException();
+		int hiddenNeurons = HiddenNeuronCount;
+		for (int iNeuron = 0; iNeuron < hiddenNeurons; iNeuron++) {
+			HiddenNeuron neuron = HiddenNeuronAt(iNeuron);
+			neuron.ComputeOutput(BuildNeuronInput(neuron, networkInput), true);
+		}
+		double[] output = new double[OutputLength];
+		for (int iNeuron = 0; iNeuron < output.Length; iNeuron++) {
+			InputDescribedNeuron neuron = OutputNeuronAt(iNeuron);
+			output[iNeuron] = neuron.ComputeOutput(BuildNeuronInput(neuron, networkInput));
+		}
+		return output;
 	}
 
 	public abstract int InputLength { get; }
 
 	public abstract int OutputLength { get; }
 
+	public override string ToString() {
+		int hiddens = HiddenNeuronCount, outputs = OutputLength;
+		StringBuilder sb = new(GetType().Name + '{' +
+			$"inputs:{InputLength},HiddenNeurons[{hiddens}]:[");
+		for (int i = 0; i < hiddens; i++) sb.Append($"#{i}:{HiddenNeuronAt(i)},");
+		sb.Remove(sb.Length - 1, 1).Append($"],OutputNeurons[{outputs}]:[");
+		for (int i = 0; i < outputs; i++) sb.Append($"#{i}:{OutputNeuronAt(i)},");
+		return sb.Remove(sb.Length - 1, 1).Append("]}").ToString();
+	}
 
 }
 
