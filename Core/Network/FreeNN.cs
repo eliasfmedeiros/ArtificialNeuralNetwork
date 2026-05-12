760e83de6db024452745f9e0f86c44b59d371775
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-05-11 23:48:59 -0300

    refactor: move free neuron types into FreeNN

--- Core/Network/FreeNN.cs
+++ Core/Network/FreeNN.cs
@@ -55,6 +55,41 @@ public abstract partial class FreeNN : INetwork {
 		return sb.Remove(sb.Length - 1, 1).Append("]}").ToString();
 	}
 
+	#region Subclasses
+	public enum InputSourceType { NetworkInput, HiddenNeuron, AuxInput }
+
+	public abstract class InputDescribedNeuron : Neuron {
+		public interface IInputDescriptor {
+			InputSourceType SourceType { get; }
+			int IndexInSource { get; }
+			string ToString();
+		}
+		public abstract IInputDescriptor DescribeInput(int inputIndex);
+
+		public override string ToString() {
+			int inputLength = InputLength;
+			StringBuilder sb = new(GetType().Name + '{' + $"bias:{GetBiasValue()},inputs[{inputLength}]:[");
+			for (int i = 0; i < inputLength; i++)
+				sb.Append($"#{i}:" + '{' + $"ref:{DescribeInput(i)},weight:{GetWeightValue(i)}" + "},");
+			return sb.Remove(sb.Length - 1, 1).Append("]}").ToString();
+		}
+	}
+
+	public abstract class HiddenNeuron(double precomputedOutput = 0) : InputDescribedNeuron {
+		public double ComputeOutput(double[] input, bool storeOutput) {
+			return storeOutput ? (StoredOutput = ComputeOutput(input)) : ComputeOutput(input);
+		}
+		public double StoredOutput { get; private set; } = precomputedOutput;
+
+		public override string ToString() {
+			string baseStr = base.ToString(), addStr = $", StoredOutput:{StoredOutput}";
+			StringBuilder sb = new StringBuilder(baseStr.Length + addStr.Length).Append(baseStr);
+			return sb.Insert(sb.Length - 1, addStr).ToString();
+		}
+	}
+
+	#endregion
+
 }
 
 
