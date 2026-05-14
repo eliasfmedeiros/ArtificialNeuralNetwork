6bb4419c6591aafad1ed5c9f96de1da2e813a9f3
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-05-14 01:11:07 -0300

    feat: add ReLU activation function
    Representation of the ReLU activation function that implements the IActivation interface, with shared instance support.

---
+++ DefaultImplementation/Function/ReLU.cs
@@ -0,0 +1,17 @@
+using EliasFM.ArtificialNeuralNet.Core;
+
+namespace EliasFM.ArtificialNeuralNet.CommonImplementation.Function;
+
+public class ReLU : IActivation {
+
+	public static readonly ReLU Shared = new();
+
+	public virtual double GetLeak() => 0;
+
+	public static double Function(double x, double leak = 0) => x < 0 ? leak * x : x;
+
+	public double Apply(double input) => Function(input, GetLeak());
+
+	public override string ToString() => GetType().Name;
+
+}
