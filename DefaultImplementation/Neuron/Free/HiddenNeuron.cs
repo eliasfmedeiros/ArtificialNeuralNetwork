3a8741bc66f3bbded0faf8f839beb6f4e268509f
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-06-07 00:33:41 -0300

    feat: add HiddenNeuron abstract class
    Adds an abstract partial implementation of "Core.Network.FreeNN.HiddenNeuron " that concretizes weight storage using a "SignalChannel" array.
    Also adds weight management methods.

---
+++ DefaultImplementation/Neuron/Free/HiddenNeuron.cs
@@ -0,0 +1,54 @@
+using EliasFM.ArtificialNeuralNet.CommonImplementation.Neuron.Free.Auxiliary;
+using EliasFM.ArtificialNeuralNet.Core;
+using EliasFM.ArtificialNeuralNet.Core.Network;
+
+namespace EliasFM.ArtificialNeuralNet.CommonImplementation.Neuron.Free;
+
+public abstract class HiddenNeuron : FreeNN.HiddenNeuron {
+	public SignalChannel[] ChannelArr { get; private set; }
+	private double bias;
+
+	public HiddenNeuron(IActivation activation, IInputDescriptor[] input, Random? weightStarter = null
+			, double precomputedOutput = 0) : base(precomputedOutput) {
+		ChannelArr = new SignalChannel[input.Length];
+		for (int i = 0; i < input.Length; i++) ChannelArr[i] = new SignalChannel(input[i], weightStarter);
+		bias = DefaultStartingWeight(weightStarter);
+	}
+	public HiddenNeuron(IActivation activator, SignalChannel[] input, double bias
+			, double precomputedOutput = 0) : base(precomputedOutput) {
+		ChannelArr = input;
+		this.bias = bias;
+	}
+
+	public override int InputLength => ChannelArr.Length;
+
+	public override double GetWeightValue(int index) =>
+		index == InputLength ? GetBiasValue() : ChannelArr[index].Weight;
+
+	public override IInputDescriptor DescribeInput(int index) => ChannelArr[index].InputDescriptor;
+
+	public override double GetBiasValue() => bias;
+
+	public double SetBias(double value) => bias = value;
+
+	public virtual int BiasIndex => InputLength;
+
+	public void AddInputChannel(SignalChannel[] addInput) {
+		SignalChannel[] newArr = new SignalChannel[ChannelArr.Length + addInput.Length];
+		for (int i = 0; i < ChannelArr.Length; i++) newArr[i] = ChannelArr[i];
+		for (int i = 0; i < addInput.Length; i++) newArr[ChannelArr.Length + i] = addInput[i];
+		ChannelArr = newArr;
+	}
+
+	public void RemoveInputChannel(int iIndex, byte range = 1) {
+		SignalChannel[] newArr = new SignalChannel[ChannelArr.Length - range];
+		for (int i = 0; i < iIndex; i++) newArr[i] = ChannelArr[i];
+		for (int i = iIndex + range; i < ChannelArr.Length; i++) newArr[i - range] = ChannelArr[i];
+		ChannelArr = newArr;
+	}
+
+	#region subclasses
+	//TODO
+	#endregion
+
+}
