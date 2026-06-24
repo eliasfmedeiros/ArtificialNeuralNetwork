d4ba148719c023ca22200184df6a05a70434a12b
Elias Medeiros <80804875+eliasfmedeiros@users.noreply.github.com>
2026-06-24 16:00:31 -0300

    feat: add "MultiLayer.OutputDescriptor" struct

--- DefaultImplementation/Network/MultiLayer.cs
+++ DefaultImplementation/Network/MultiLayer.cs
@@ -1,4 +1,5 @@
 
+using EliasFM.ArtificialNeuralNet.Core;
 using EliasFM.ArtificialNeuralNet.Core.Network;
 using MultiLayerBase = EliasFM.ArtificialNeuralNet.Core.Network.MultiLayer;
 
@@ -10,10 +11,21 @@ public class MultiLayer : MultiLayerBase {
 	public MultiLayer(INetwork[] layerArr) {
 		this.layerArr = layerArr;
 	}
+	public MultiLayer(int input, OutputDescriptor[] descriptorArr) {
+		layerArr = new INetwork[descriptorArr.Length];
+		for (int i = 0; i < descriptorArr.Length; i++) {
+			layerArr[i] = new SingleLayer(input, descriptorArr[i].ActivatorArr);
+			input = layerArr[i].OutputLength;
+		}
+	}
 
 	public override int LayerCount => layerArr.Length;
 
 	public override INetwork LayerAt(int index) {
 		return layerArr[index];
+	}
+
+	public readonly struct OutputDescriptor {
+		public readonly IActivation[] ActivatorArr;
 	}
 }
